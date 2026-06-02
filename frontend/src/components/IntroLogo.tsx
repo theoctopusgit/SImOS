6e469a78e40419d34f92d809bc08e9ff54e53c0d
RodneyGG <rodneyarevalo9963@gmail.com>
2026-06-02 11:00:50 +0800

    added the intro for the website

---
+++ frontend/src/components/IntroLogo.tsx
@@ -0,0 +1,14 @@
+import "./IntroLogo.css";
+
+function IntroLogo() {
+    return (
+        <div className="intro-container">
+            <h1 className="logo">
+                <span className="sim">SIm</span>
+                <span className="os">OS</span>
+            </h1>
+        </div>
+    );
+}
+
+export default IntroLogo;
